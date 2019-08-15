7558b6bc2b4e8a673c917096ae43b955e98d4174
Muhammad Usman <42235538+mail2chromium@users.noreply.github.com>
2019-08-15 06:27:45 +0500

    Create __init__.py
    Starting Point

---
+++ Classifier/__init__.py
@@ -0,0 +1,6 @@
+from GenericPredictiveAnalysis import PredictiveAnalysis
+
+if __name__ == '__main__':
+
+    obj_nlp = PredictiveAnalysis()
+    obj_nlp.main_processing(file_name='Visitors_Reviews.tsv')
